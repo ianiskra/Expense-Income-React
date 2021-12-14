6a7979df1f745352a88b3004d79a271883f07864
Ian Iskra <ianiskra@gmail.com>
2021-12-14 00:19:11 -0800

    Minor changes on UI at the input fields

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -25,9 +25,9 @@ export const AddTransaction = () => {
             <h3>Add new transaction</h3>
             <form onSubmit={onSubmit}>
                 <div className="form-control">
-                    <label htmlFor="text">Text</label>
+                    <label htmlFor="text">Item Transacted</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} 
-                    placeholder="Enter text..." />
+                    placeholder="Your Transaction..." />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">Amount <br />
